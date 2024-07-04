ba7f000c316716c0bb780e3bf4af5f6d65dc9c72
lejos123 <lejosdelapatriamia1@gmail.com>
2024-07-04 19:05:20 -0300

    hoja style y app modificados- arreglo desborde v0
    se agrega el overflow

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 document.getElementById('playButton').addEventListener('click', function() {
-    // Reproducir el audio
     var audio = document.getElementById('audio');
     audio.play();
 
-    // Ocultar la alerta
     document.getElementById('alertBox').style.display = 'none';
-    
-    // Mostrar el contenido
     document.getElementById('content').style.display = 'block';
-
 });
 
-document.getElementById('pauseButton').addEventListener('click', function(){
+document.getElementById('pauseButton').addEventListener('click', function() {
     var audio = document.getElementById('audio');
     audio.pause();
 });
